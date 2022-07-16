77253ba3f3b8ce38903af525e23ae02c8456a07d
Babenko Liudmyla <savelieva0509@gmail.com>
2022-07-16 23:32:11 +0300

    Фон для секции и модалка с адресами

--- src/index.js
+++ src/index.js
@@ -10,3 +10,21 @@ new Swiper('.rewiews-slider', {
     clickable: true,
   },
 });
+
+// SCRIPT_CONTACTS_MODAL_LOCATION
+
+(() => {
+  const refs = {
+    openLocationBtn: document.querySelector("[data-location-open]"),
+    closeLocation: document.querySelector("[data-location-close]"),
+    location: document.querySelector("[data-location]"),
+  };
+
+  refs.openLocationBtn.addEventListener("click", toggleLocation);
+  refs.closeLocationBtn.addEventListener("click", toggleLocation);
+
+  function toggleLocation() {
+    refs.location.classList.toggle("is-hidden");
+    refs.body.classList.toggle('no-scroll');
+  }
+})();
