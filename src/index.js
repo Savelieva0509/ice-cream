0a617e3997070ff3e3b8a6d8fc92258fbc66cc8d
Babenko Liudmyla <savelieva0509@gmail.com>
2022-07-19 00:42:39 +0300

    Правки обе модалки + валидатор

--- src/index.js
+++ src/index.js
@@ -11,17 +11,36 @@ new Swiper('.rewiews-slider', {
   },
 });
 
+// SCRIPT_CONTACTS_MODAL_FRANCHISE
+
+(() => {
+  const refs = {
+    openFranchiseBtn: document.querySelector("[data-franchise-open]"),
+    closeFranchiseBtn: document.querySelector("[data-franchise-close]"),
+    franchise: document.querySelector("[data-franchise]"),
+  };
+
+  refs.openFranchiseBtn.addEventListener("click", toggleFranchise);
+  refs.closeFranchiseBtn.addEventListener("click", toggleFranchise);
+
+  function toggleFranchise() {
+    refs.franchise.classList.toggle("is-hidden");
+    refs.body.classList.toggle('no-scroll');
+  }
+})();
+
+
 // SCRIPT_CONTACTS_MODAL_LOCATION
 
 (() => {
   const refs = {
     openLocationBtn: document.querySelector("[data-location-open]"),
-    closeLocation: document.querySelector("[data-location-close]"),
+    closeLocationBtn: document.querySelector("[data-location-close]"),
     location: document.querySelector("[data-location]"),
   };
 
   refs.openLocationBtn.addEventListener("click", toggleLocation);
-  refs.openLocationBtn.addEventListener("click", toggleLocation);
+  refs.closeLocationBtn.addEventListener("click", toggleLocation);
 
   function toggleLocation() {
     refs.location.classList.toggle("is-hidden");
@@ -29,21 +48,4 @@ new Swiper('.rewiews-slider', {
   }
 })();
 
-// SCRIPT_CONTACTS_MODAL_FRANCHISE
-
-(() => {
-  const refs = {
-    openFranchiseBtn: document.querySelector("[data-franchise-open]"),
-    closeFranchise: document.querySelector("[data-franchise-close]"),
-    franchise: document.querySelector("[data-franchise]"),
-  };
-
-  refs.openFranchiseBtn.addEventListener("click", toggleFranchise);
-  refs.openFranchiseBtn.addEventListener("click", toggleFranchise);
-
-  function toggleFranchise() {
-    refs.franchise.classList.toggle("is-hidden");
-    refs.body.classList.toggle('no-scroll');
-  }
-})();
 
