eccc30024c88cd4100cfa8430f8b1eb68a1b77e0
Serhii-Ter <serhij86@gmail.com>
2022-07-20 00:34:34 +0300

    small bugs fixed

--- src/slider.js
+++ src/slider.js
@@ -1,23 +1,15 @@
-// Swiper initialisation
 new Swiper('.rewiews-slider', {
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-    dynamicBullets: true,
-  },
+
   keyboard: {
     enabled: true,
     onlyInViewport: true,
     pageUpDown: true,
   },
-  spaceBetween: 50,
+
   speed: 1000,
   autoheight: true,
-  simulateTouch: true,
-  touchAngle: 45,
-  grabCursor: true,
 });
