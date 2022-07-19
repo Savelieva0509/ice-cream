642635a5b76954ace1d9f9d6f279cd3620ab7537
Nastyona22 <nastyona.savina@gmail.com>
2022-07-19 18:32:50 +0300

    Update mob-menu.js

--- src/mob-menu.js
+++ src/mob-menu.js
@@ -2,7 +2,7 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
-  // const mobList = document.querySelect('.js-menu-list');
+  const mobList = document.querySelect('.js-menu-list');
   const toggleMenu = () => {
     const isMenuOpen =
       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
@@ -15,7 +15,7 @@
   };
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
-  // mobList.addEventListener('click', toggleMenu);
+  mobList.addEventListener('click', toggleMenu);
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
