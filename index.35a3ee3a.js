bb601829f7967481a4171d17925937a67ea83c0c
Savelieva0509 <104641072+Savelieva0509@users.noreply.github.com>
2024-01-11 12:57:24 +0000

    Deploying to gh-pages from @ Savelieva0509/ice-cream@2fc5d6e50a5831558398f1a7d8ca9753a8a67563 🚀

---
+++ index.35a3ee3a.js
@@ -0,0 +1,2 @@
+new Swiper(".rewiews-slider",{navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},keyboard:{enabled:!0,onlyInViewport:!0,pageUpDown:!0},speed:1e3,autoheight:!0});
+//# sourceMappingURL=index.35a3ee3a.js.map
